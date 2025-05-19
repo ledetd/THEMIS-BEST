238d37d21596d0c2832bc22bec945fec5dc884ff
Dwight Ledet <dwightledet@protonmail.com>
2025-05-19 16:30:09 +0200

    footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="bg-white">
+            <div className="px-4 sm:px-6 lg:px-8">
+
+                <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24">
+                    <div className="flex gap-x-6 md:order-2">
+
+                    </div>
+                    <p className="mt-8 text-sm/6 text-gray-600 md:order-1 md:mt-0">
+                        &copy; 2025 BEST All rights reserved.
+                    </p>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
--- frontend/src/components/WellListings.jsx
+++ frontend/src/components/WellListings.jsx
@@ -59,6 +59,7 @@ const WellListings = () => {
                         <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
                           {well.project_name}
                         </td>
+
                       </tr>
                     )
                     }
